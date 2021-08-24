6da8dbb7e0cbbd1ce937d07990c111b3a6b9591b
wooky9633 <wooky9633@naver.com>
2021-08-25 02:08:09 +0900

    spring init

---
+++ studyMatcher/src/main/java/studyMatcher/studyMatcher/StudyMatcherApplication.java
@@ -0,0 +1,13 @@
+package studyMatcher.studyMatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMatcherApplication.class, args);
+	}
+
+}
---
+++ studyMatcher/src/test/java/studyMatcher/studyMatcher/StudyMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package studyMatcher.studyMatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
