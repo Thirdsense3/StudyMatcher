e12b72ece2c9224d3b9ec38dbf68f76dc482e4b8
DESKTOP-54PSM5B\wooky <wooky9633@gmail.com>
2021-09-04 13:51:12 +0900

    Spring 테스트 코드 작성

--- studyMatcherSpring/src/test/java/studyMatcherSpring/studyMatcherSpring/member/MemberServiceTest.java
+++ studyMatcherSpring/src/test/java/studyMatcherSpring/studyMatcherSpring/member/MemberServiceTest.java
@@ -1,6 +1,9 @@
 package studyMatcherSpring.studyMatcherSpring.member;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,20 +15,19 @@ import studyMatcherSpring.studyMatcherSpring.dao.Member;
 import studyMatcherSpring.studyMatcherSpring.service.MemberService;
 
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
 
     @Test
-    @Transactional
-    @Rollback(false)
     public void save() {
         // given
         Member member = new Member();
-        member.setEmail("wooky9633@naver.com");
+        member.setNickname("wooky9633");
         member.setName("kong");
-        member.setPassword("288288");
+        member.setPassword("1234");
         Location location = new Location("인천", "연수");
         member.setLocation(location);
         member.setLevel(Level.PLATINUM);
@@ -35,5 +37,80 @@ class MemberServiceTest {
 
         // then
         assertThat(saveMember).isEqualTo(member);
+    }
+
+    @Test
+    public void duplicateCheck() {
+        // given
+        Member member = new Member();
+        member.setNickname("wooky9633");
+        member.setName("kong");
+        member.setPassword("1234");
+        Location location = new Location("인천", "연수");
+        member.setLocation(location);
+        member.setLevel(Level.PLATINUM);
+        memberService.save(member);
+
+        // when
+        Boolean found = memberService.validateDuplicatedNickname(member.getNickname());
+        Boolean notFound = memberService.validateDuplicatedNickname("");
+
+        // then
+        assertThat(found).isEqualTo(false);
+        assertThat(notFound).isEqualTo(true);
+    }
+
+    @Test
+    public void login() {
+        // given
+        Member member = new Member();
+        member.setNickname("wooky9633");
+        member.setName("kong");
+        member.setPassword("1234");
+        Location location = new Location("인천", "연수");
+        member.setLocation(location);
+        member.setLevel(Level.PLATINUM);
+        memberService.save(member);
+
+        Member loginMember = new Member();
+        loginMember.setNickname("wooky9633");
+        loginMember.setPassword("1234");
+
+        Member wrongEmail = new Member();
+        wrongEmail.setNickname("wooky9633@naver.com");
+        wrongEmail.setPassword("1234");
+
+        Member wrongPassword = new Member();
+        wrongPassword.setNickname("wooky9633");
+        wrongPassword.setPassword("1233");
+
+        // when
+        Long resultId = memberService.login(loginMember);
+        Long wrong1 = memberService.login(wrongEmail);
+        Long wrong2 = memberService.login(wrongPassword);
+
+        // then
+        assertThat(resultId).isEqualTo(member.getId());
+        assertThat(wrong1).isEqualTo(0L);
+        assertThat(wrong2).isEqualTo(0L);
+    }
+
+    @Test
+    public void getMemberInform() {
+        //given
+        Member member = new Member();
+        member.setNickname("wooky9633");
+        member.setName("kong");
+        member.setPassword("1234");
+        Location location = new Location("인천", "연수");
+        member.setLocation(location);
+        member.setLevel(Level.PLATINUM);
+        memberService.save(member);
+
+        //when
+        Member memberInform = memberService.getMemberInform(member.getId());
+
+        //then
+        assertThat(member).isEqualTo(memberInform);
     }
 }
