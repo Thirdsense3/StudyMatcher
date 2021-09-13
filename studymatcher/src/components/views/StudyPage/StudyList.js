e8dd6aae320e6a66e1f388d3ba6fa563f6990245
Thirdsense <31949361+Thirdsense3@users.noreply.github.com>
2021-09-13 21:44:15 +0900

    studyList js 추가

---
+++ studymatcher/src/components/views/StudyPage/StudyList.js
@@ -0,0 +1,14 @@
+import Header from "../../layout/Header";
+import Footer from "../../layout/Footer";
+
+import React, { useState } from "react";
+
+export default function StudyList() {
+
+    const [study, setStudy] = useState("");
+
+    return (
+        <Header />
+        
+    )
+}
