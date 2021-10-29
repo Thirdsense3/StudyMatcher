59f281a2c20201270c478c8a2450ebee9846476b
JihoonKwak <kkwakjh@naver.com>
2021-10-29 13:57:40 +0900

    서버 URL 관리 방식 수정 및 검색 API 추가

--- studymatcher/src/_reducers/user_reducer.js
+++ studymatcher/src/_reducers/user_reducer.js
@@ -1,6 +1,7 @@
 import {
   LOGIN_USER,
-  REGISTER_USER
+  REGISTER_USER,
+  SEARCH_STUDY
 } from '../_actions/types';
 
 export default (state = {}, action) => {
@@ -9,6 +10,8 @@ export default (state = {}, action) => {
       return { ...state, loginSuccess: action.payload }
     case REGISTER_USER:
       return { ...state, register: action.payload }
+    case SEARCH_STUDY:
+      return { ...state, payload: action.payload }
     default:
       return state;
   }
