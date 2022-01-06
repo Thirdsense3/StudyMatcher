70108c454a8c9891b400f62684f72a2d91610d78
JihoonKwak <kkwakjh@naver.com>
2022-01-06 22:11:32 +0900

    [Header] 로그인 상태 구현

--- studymatcher/src/components/elements/ui/HeaderTop.js
+++ studymatcher/src/components/elements/ui/HeaderTop.js
@@ -1,13 +1,76 @@
 import { Link } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 import { logoutUser } from '../../../_actions/user_action';
 
 export default function HeaderTop() {
   const dispatch = useDispatch();
+  const store = useStore();
 
   const onClickLogoutHandler = () => {
     dispatch(logoutUser());
     alert('로그아웃 되셨습니다.');
+  }
+
+  const onClickEditHandler = () => {
+    // dispatch(editUser());
+    console.log('회원정보수정 버튼 클릭');
+  }
+
+  const onClickCheckLoggedHandler = () => {
+    if (store.getState().user.logged === true) {
+      console.log(store.getState().user.loggedInfo);
+    } else {
+      console.log(store.getState());
+    }
+  }
+
+  const LoginState = () => {
+    if (store.getState().user.logged === true) {
+      return(
+        <div className="same-language-currency language-style">
+          <span>
+            {store.getState().user.loggedInfo.id}
+          </span>
+          <div className="lang-car-dropdown">
+            <ul>
+              <li>
+                <Link to="./">
+                  <button value="logout" onClick={onClickLogoutHandler}>로그아웃</button>
+                </Link>
+              </li>
+              <li>
+                <button value="editUser" onClick={onClickEditHandler}>회원정보수정</button>
+              </li>
+              <li>
+                <button value="checkLogged" onClick={onClickCheckLoggedHandler}>로그인 체크</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )
+    } else {
+      return(
+        <div className="same-language-currency language-style">
+          <span>
+            회원 <i className="fa fa-angle-down"></i>
+          </span>
+          <div className="lang-car-dropdown">
+            <ul>
+              <li>
+                <Link to="./login">
+                  <button value="login">로그인</button>
+                </Link>
+              </li>
+              <li>
+                <Link to="./register">
+                  <button value="register">회원가입</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )
+    }
   }
 
   return (
@@ -54,6 +117,9 @@ export default function HeaderTop() {
                   <li>
                     <button value="logout" onClick={onClickLogoutHandler}>로그아웃</button>
                   </li>
+                  <li>
+                    <button value="checkLogged" onClick={onClickCheckLoggedHandler}>로그인 체크</button>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -71,6 +137,7 @@ export default function HeaderTop() {
                 </Link>
               </span>
             </div>
+            <LoginState />
           </div>
         </div>
       </div>
